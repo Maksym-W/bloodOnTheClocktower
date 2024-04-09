12b2bcd45b5183434d83a17a5626720d36712d1d
Max <m.woychyshyn@mail.utoronto.ca>
2024-04-09 18:50:50 -0400

    Made progress on roles

--- src/roles.py
+++ src/roles.py
@@ -1,31 +1,51 @@
-# The purpose of this file is to establish the roles of the game. This will be imported in the main file
-# TODO Figure out if you should be importing the database here or in the server
-# TODO Figure out if there is a distinction between townsfolk and outsides
+# The purpose of this file is to establish the roles of the game.
+# This will be imported in the main file
+import sqlite3  # This is to get data for the roles
 
-class Town():
-	def __init__(self):
-		self.alligence = "Townsfolk"
-		self.guarded = False # Can change if Soldier/guarded by Monk
 
-class Minions():
+class Skeleton_role():
 	def __init__(self):
-		self.alligence = "Minions"
+		pass
+
+	def get_ability(self, role_name):
+		# Connect to the SQLite database
+		conn = sqlite3.connect('game.db')
+		cursor = conn.cursor()
+
+		# Execute a query to get the ability based on the role name
+		cursor.execute("SELECT ability FROM roles WHERE name = ?", (role_name,)) # TODO FIX THIS QUERY
+		result = cursor.fetchone()
+
+		# Close the database connection
+		conn.close()
+
+		if result:
+			return result[0]  # Return the ability
+		else:
+			return None  # If no ability is found, return None
 
 # Now what is below are the specific town roles. They will inherit the stuff above
 
-class Soldier(Town):
-	def __init__(self):
-		self.guarded = True
 
-class Slayer(Town):
+class Soldier(Skeleton_role):
 	def __init__(self):
+		super().__init__()  # Initialize the base class
+		self.guarded = True
+		self.ability = self.get_ability("Soldier")  # Use the role name "Soldier" to fetch its ability
+
+
+class Slayer(Skeleton_role):
+	def __init__(self):
+		super().__init__()
 		self.target = None
 
 	def target(self, target):
 		self.target = target
-		if target.alligence = "Minions":
+		if target.alligence == "Minions":
 			target.kill()
 
-class Drunk(Town):
+
+class Drunk(Skeleton_role):
 	def __init__(self, percieved_role):
+		super().__init__()
 		self.percieved_role = percieved_role
